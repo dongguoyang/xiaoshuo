66cfa0931e5a374c5ae98381ad2cf2ad96753363
huangc <342191298@qq.com>
2020-11-14 12:38:21 +0800

    修改bug

--- app/Logics/Repositories/src/ReadNovelLogsRepository.php
+++ app/Logics/Repositories/src/ReadNovelLogsRepository.php
@@ -11,9 +11,9 @@ class ReadNovelLogsRepository  extends Repository {
     }
 
     public function AddNumLog($customer_id,$platform_wechat_id,$novel_id,$novel_section_id,$novel){
-        $result = $this->model->findByMap([['customer_id'=>$customer_id],['novel_id'=>$novel_id],['novel_section_id'=>$novel_section_id]],$this->model->fields);
+        $result = $this->findByMap([['customer_id'=>$customer_id],['novel_id'=>$novel_id],['novel_section_id'=>$novel_section_id]],$this->model->fields);
         if($result){ //数量加一
-            $this->model->update(['user_read_num'=>$result['user_read_num']+1],$result['id']);
+            $this->update(['user_read_num'=>$result['user_read_num']+1],$result['id']);
         }else{ //新增数据
             $data = [
                'customer_id'=> $customer_id,
@@ -24,7 +24,7 @@ class ReadNovelLogsRepository  extends Repository {
                'user_read_num'=>1,
                'title'=>$novel['section_title']
             ];
-            $this->model->create($data);
+            $this->create($data);
         }
     }
 
